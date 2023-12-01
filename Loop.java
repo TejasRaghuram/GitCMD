8de932ee757b5f36baa505e205ecb1dbcd0d715d
Tejas Raghuram <10018114@sbstudents.org>
2023-12-01 11:55:40 -0500

    While Loop

--- Loop.java
+++ Loop.java
@@ -2,9 +2,11 @@ public class Loop
 {
     public static void main(String[] args)
     {
-        for(int i = 1; i <= 10; i++)
+        int i = 1;
+        while(i <= 10)
         {
             System.out.println(i);
+            i++;
         }
     }
 }
